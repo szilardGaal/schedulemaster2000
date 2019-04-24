7922501f7d84d1c2cb2ee168fc1ac0cd45262926
NagyTamy <nagy.tamy@gmail.com>
2019-04-24 13:52:51 +0200

    Autoupdate slots when tasks added on begin date

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -3,11 +3,11 @@
 */
 DO $$ BEGIN
 
-DROP TABLE IF EXISTS slots;
-DROP TABLE IF EXISTS tasks;
-DROP TABLE IF EXISTS schedule_columns;
-DROP TABLE IF EXISTS schedules;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS slots CASCADE;
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TABLE IF EXISTS schedule_columns CASCADE;
+DROP TABLE IF EXISTS schedules CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -23,7 +23,7 @@ CREATE TABLE schedules(
     title text NOT NULL,
     numOfCol int NOT NULL,
     isPublic boolean DEFAULT false,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT limited_num_of_col CHECK ( numOfCol > 0 AND numOfCol <= 7 ),
     CONSTRAINT title_not_empty CHECK ( title <> '' )
 );
@@ -32,7 +32,7 @@ CREATE TABLE schedule_columns(
     id SERIAL PRIMARY KEY,
     schedule_id int,
     title varchar(150) NOT NULL,
-    FOREIGN KEY (schedule_id) REFERENCES schedules(id),
+    FOREIGN KEY (schedule_id) REFERENCES schedules(id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT title_not_empty CHECK ( title <> '' )
 );
 
@@ -42,27 +42,27 @@ CREATE TABLE tasks(
     title varchar(150) NOT NULL,
     content text,
     begins int,
-    ends int,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    duration int,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT title_not_empty CHECK ( title <> '' ),
     CONSTRAINT limit_begins CHECK ( begins >= 1 AND begins <= 24 ),
-    CONSTRAINT limit_ends CHECK ( ends >= 1 AND ends <= 24 AND ends > begins )
+    CONSTRAINT limit_ends CHECK ( duration >= 1 AND duration <= 24-begins )
 );
 
 CREATE TABLE slots(
     column_id int,
     task_id int DEFAULT null,
     time varchar(5),
-    FOREIGN KEY (column_id) REFERENCES schedule_columns(id),
-    FOREIGN KEY (task_id) REFERENCES tasks(id)
+    FOREIGN KEY (column_id) REFERENCES schedule_columns(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE OR REPLACE FUNCTION create_columns_on_schedule() RETURNS TRIGGER AS $A$
     DECLARE
-        count int := 1;
+        count int := 0;
     BEGIN
         LOOP
-            INSERT INTO schedule_columns (schedule_id, title) VALUES (NEW.id, 'Day '||count);
+            INSERT INTO schedule_columns (schedule_id, title) VALUES (NEW.id, 'Day '||count+1);
             count := count + 1;
             EXIT WHEN count = NEW.numOfCol;
         end loop;
@@ -100,14 +100,39 @@ CREATE TRIGGER slots_on_columns
 EXECUTE PROCEDURE create_slots_on_columns();
 
 
+CREATE OR REPLACE FUNCTION add_tasks_to_slots() RETURNS TRIGGER AS $A$
+BEGIN
+    IF NEW.time != ((SELECT begins FROM tasks WHERE id=NEW.task_id)+(SELECT duration FROM tasks WHERE id=NEW.task_id)-1)||':00' THEN
+        UPDATE slots SET task_id=NEW.task_id WHERE column_id = NEW.column_id AND time = ((split_part(NEW.time, ':', 1))::int)+1||':00';
+    end if;
+    RETURN NEW;
+END; $A$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER task_on_slots
+    AFTER UPDATE
+    ON slots
+    FOR EACH ROW
+EXECUTE PROCEDURE  add_tasks_to_slots();
+
+
+
+
+
+
 INSERT INTO users(name, password) VALUES ('test', 'test');
 
 INSERT INTO schedules(user_id, title, numOfCol) VALUES (1, 'title', '2');
 INSERT INTO schedules(user_id, title, numOfCol) VALUES (1, 'title', '3');
 
-INSERT INTO tasks(user_id, title, content) VALUES (1, 'task title', 'task content');
+INSERT INTO tasks(user_id, title, content, begins, duration) VALUES (1, 'task title', 'task content', 4, 2);
+INSERT INTO tasks(user_id, title, content, begins, duration) VALUES (1, 'task title', 'task content', 7, 3);
+INSERT INTO tasks(user_id, title, content, begins, duration) VALUES (1, 'task title', 'task content', 13, 4);
 
-UPDATE slots SET task_id = 1 WHERE column_id = 1 AND time = '1:00';
+UPDATE slots SET task_id = 1 WHERE column_id = 1 AND time = (SELECT begins FROM tasks WHERE id = 1)||':00';
+UPDATE slots SET task_id = 2 WHERE column_id = 1 AND time = (SELECT begins FROM tasks WHERE id = 2)||':00';
+UPDATE slots SET task_id = 3 WHERE column_id = 1 AND time = (SELECT begins FROM tasks WHERE id = 3)||':00';
+
 
 
 END $$
